686f0749fdf0fc116cf79b10353e672fec547d77
naemulislamdev <naemulislam.dev@gmail.com>
2024-07-02 18:14:14 +0600

    add customer review

--- assets/js/owl-extra-code.js
+++ assets/js/owl-extra-code.js
@@ -22,14 +22,14 @@ $('.categories').owlCarousel({
     }
 });
 //header miniSlider Slider
-$('.miniSlider').owlCarousel({
+$('.c-review-slider').owlCarousel({
     loop: true,
     margin: 10,
     autoplay: true,
-    autoplayTimeout: 3000,
+    autoplayTimeout: 5000,
     autoplayHoverPause: true,
     nav: false,
-    dots: false,
+    dots: true,
     responsive: {
         0: {
             items: 1
@@ -38,59 +38,7 @@ $('.miniSlider').owlCarousel({
             items: 1
         },
         1000: {
-            items: 1
-        }
-    }
-});
-
-//Campas Owl carosul code
-$('.campus-owl').owlCarousel({
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
-        },
-        600:{
-            items:1,
-            nav:false
-        },
-        1000:{
-            items:1,
-            nav:false,
-            loop:true
-        }
-    }
-});
-
-
-//publication Owl carosul code
-$('.gallery-owl').owlCarousel({
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:false,
-
-        },
-        600:{
-            items:3,
-            nav:false
-        },
-        1000:{
-            items:4,
-            nav:false,
-            loop:true
+            items: 2
         }
     }
 });
